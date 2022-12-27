e40a2a48d433d4e87bc3d8801008a0c3268242d4
Puthpiseth <puthpiseth_tun@yahoo.com>
2022-12-27 01:12:52 +0100

    fix: refactor code

--- src/components/ui/text/ErrorSpan.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const ErrorSpan = styled.span`
-  display: flex;
-  justify-content: flex-start;
-  width: 100%;
-  color: #ff5f59;
-  font-size: 12px;
-`;
